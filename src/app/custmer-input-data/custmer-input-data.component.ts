ba3bdc282973ea0d8fd76d2252a2c9ecdc3d9ad3
KrupaliSanghani <krupalisanghani.ownai@gmail.com>
2022-06-29 19:34:23 +0530

    complete

--- src/app/custmer-input-data/custmer-input-data.component.ts
+++ src/app/custmer-input-data/custmer-input-data.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, } from '@angular/core';
-import { subscribeOn } from 'rxjs';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { CustomerDisplayDataComponent } from '../customer-display-data/customer-display-data.component';
+import { ChangeDetectorRef } from '@angular/core';
 // import { CustomerDataService } from '../customer-data.service';
 
 
@@ -9,8 +10,8 @@ import { subscribeOn } from 'rxjs';
   templateUrl: './custmer-input-data.component.html',
   styleUrls: ['./custmer-input-data.component.css']
 })
-export class CustmerInputDataComponent implements OnInit {
-
+export class CustmerInputDataComponent implements OnInit, AfterViewInit {
+  @ViewChild(CustomerDisplayDataComponent) CustomerDisplayDataComponent!: CustomerDisplayDataComponent;
 
   // ------take inputs------------
   customerName: string = '';
@@ -30,15 +31,15 @@ export class CustmerInputDataComponent implements OnInit {
   existsEmail: boolean = false;
   valid: string;
   isDesc: boolean = true;
-  emailVal: string;
-  emailArr = [];
+
+
 
   // ------edit data------
   editMode: boolean = false;
 
   // -------store customer data---------
   customerArr = [];
-  customerChangeArr = [];
+  // customerChangeArr = [];
 
 
 
@@ -47,10 +48,14 @@ export class CustmerInputDataComponent implements OnInit {
     this.customerAddress = val;
   }
 
-  // constructor(private customerdata: CustomerDataService) { }
+  constructor(private cdref: ChangeDetectorRef) { }
+
+
+  ngAfterViewInit() {
+
+  }
 
   ngOnInit(): void {
-    this.sortName('customerName');
   }
 
 
@@ -59,7 +64,6 @@ export class CustmerInputDataComponent implements OnInit {
     this.valid = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
     if (this.customerEmail.match(this.valid)) {
       this.validEmail = true;
-      console.log(this.emailArr);
     }
     else {
       this.validEmail = false;
@@ -73,53 +77,50 @@ export class CustmerInputDataComponent implements OnInit {
 
 
   // ----------By default sort----------
-  sortName(property: any) {
-    var direction = this.isDesc ? 1 : -1;
-    this.customerArr.sort(function (a, b) {
-      if (a[property] < b[property]) {
-        return -1 * direction;
-      }
-      else if (a[property] > b[property]) {
-        return 1 * direction;
-      }
-      else {
-        return 0;
-      }
-    });
+
+  sort(property: any) {
+    this.CustomerDisplayDataComponent.sortData(property);
+    console.log(this.CustomerDisplayDataComponent.sortData(property));
   }
 
 
   // ------------submit customer data---------
   onSubmit() {
 
-
     // ----------check validation-----------
-    this.emailVal = this.emailArr.find(element => element == this.customerEmail);
-    this.emailArr.push(this.customerEmail);
-    console.log(this.emailVal);
+    let emailVal = this.customerArr.find(element => element.Email == this.customerEmail);
+
+    console.log(emailVal);
 
     if (this.customerEmail.match(this.valid) && this.customerName !== ' ') {
 
       // --------------for update data--------
 
       if (this.editMode) {
+        const customerChangeArr = []
+
         this.editMode = true;
-        this.customerChangeArr.push({ Name: this.customerName, Email: this.customerEmail, Status: this.customerStatus, Address: this.customerAddress });
-        this.customerArr = this.customerArr.map(obj => this.customerChangeArr.find(o => o.Email === obj.Email) || obj);
-        console.log(this.customerChangeArr);
+        customerChangeArr.push({ Name: this.customerName, Email: this.customerEmail, Status: this.customerStatus, Address: this.customerAddress });
+        this.customerArr = this.customerArr.map(obj => customerChangeArr.find(o => o.Email === obj.Email) || obj);
+        console.log(customerChangeArr);
         this.editMode = false;
       }
 
       // -------------for submit data----------
-      else if (!this.emailVal) {
+      else if (!emailVal) {
 
         // -----------pass customer data to customer display component------------
         this.customerArr.push({ Name: this.customerName, Email: this.customerEmail, Status: this.customerStatus, Address: this.customerAddress });
         // this.customerdata.addToService({ Name: this.customerName, Email: this.customerEmail, Status: this.customerStatus, Address: this.customerAddress });
         this.existsEmail = false;
         console.log(this.customerArr);
+
+        // ---------for reset value-------
+        this.customerName = '';
+        this.customerEmail = '';
+        this.customerAddress = '';
       }
-      else if (this.emailVal) {
+      else if (emailVal) {
         this.editMode = false;
         this.existsEmail = true;
       }
@@ -127,12 +128,7 @@ export class CustmerInputDataComponent implements OnInit {
 
     }
 
-    // --------display Error--------
 
-    // ---------for reset value-------
-    this.customerName = '';
-    this.customerEmail = '';
-    this.customerAddress = '';
   }
 
   // ------for Edit Data---------
@@ -146,3 +142,5 @@ export class CustmerInputDataComponent implements OnInit {
   }
 
 }
+
+
--- src/app/customer-display-data/customer-display-data.component.ts
+++ src/app/customer-display-data/customer-display-data.component.ts
@@ -16,10 +16,24 @@ export class CustomerDisplayDataComponent implements OnInit, OnChanges {
 
   displayInactiveArr = [];
   displayStatusArr = [];
+
   // -------sorting var------
   isDesc: boolean = true;
   Email: Boolean = true;
   Address: Boolean = true;
+
+  // -------show address-----
+  addClick: string;
+  clicked: boolean = false;
+
+
+  change(val) {
+    console.log(val);
+    document.getElementById(val).classList.toggle('showAdd');
+    console.log(document.getElementById(val));
+
+  }
+
 
 
   // --------status dropdown-----
@@ -43,7 +57,7 @@ export class CustomerDisplayDataComponent implements OnInit, OnChanges {
 
   // ----------- filter by status------
   onStatus() {
-    console.log(this.status);
+    // console.log(this.status);
     this.status == 'All' ? (this.displayStatusArr = this.displayArr) : (this.displayStatusArr = this.displayArr.filter(displayArr => displayArr.Status == this.status));
 
   }
@@ -56,9 +70,10 @@ export class CustomerDisplayDataComponent implements OnInit, OnChanges {
   }
 
   // ---------------sorting by Name----------
-  sortName(property: string) {
-    this.isDesc = !this.isDesc;
-    var direction = this.isDesc ? 1 : -1;
+  sortData(property: string) {
+    let direction: number;
+
+    property == 'Name' ? (this.isDesc = !this.isDesc, direction = this.isDesc ? 1 : -1) : (property == 'Email' ? (this.Email = !this.Email, direction = this.Email ? 1 : -1) : (this.Address = !this.Address, direction = this.Address ? 1 : -1));
     this.displayArr.sort(function (a, b) {
       if (a[property] < b[property]) {
         return -1 * direction;
@@ -72,40 +87,9 @@ export class CustomerDisplayDataComponent implements OnInit, OnChanges {
     });
   }
 
-  // ---------------sorting by email----------
-  sortEmail(property: string) {
-    this.Email = !this.Email;
-    var direction = this.Email ? 1 : -1;
-    this.displayArr.sort(function (a, b) {
-      if (a[property] < b[property]) {
-        return -1 * direction;
-      }
-      else if (a[property] > b[property]) {
-        return 1 * direction;
-      }
-      else {
-        return 0;
-      }
-    });
-  }
 
-  // ---------------sorting by address----------
-  sortAddress(property: string) {
-    this.Address = !this.Address;
-    var direction = this.Address ? 1 : -1;
-    this.displayArr.sort(function (a, b) {
-      if (a[property] < b[property]) {
-        return -1 * direction;
-      }
-      else if (a[property] > b[property]) {
-        return 1 * direction;
-      }
-      else {
-        return 0;
-      }
-    });
-  }
 
 }
+
 
 
